7926f7bf8d29966181eb7dad329f32888b8066ab
sydjaff <sydjaff07@gmail.com>
2023-03-02 00:25:10 +0500

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Ideas | GulAhmed</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+      integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+    <div class="main-box">
+      <!-- <div class="main-img">
+        <img src="./images/bg-img.png" />
+      </div> -->
+      <div
+        class="right-box text-center d-flex align-items-center justify-content-center"
+      >
+        <div class="content">
+          <div class="heading">
+            <h5 class="text-uppercase primary-color">
+              Our Reqruitement Process
+            </h5>
+          </div>
+          <div class="icons d-flex align-items-center">
+            <div class="icon-circle">
+              <i class="fa fa-users"></i>
+              <small>Applicant Pool</small>
+            </div>
+            <div class="icon-circle">
+              <i class="fa fa-user-tie"></i>
+              <small>1<sup>st</sup> Interview</small>
+            </div>
+            <div class="icon-circle">
+              <i class="fa fa-list-check"></i>
+              <small>Document Submission/Evaluation</small>
+            </div>
+            <div class="icon-circle">
+              <i class="fa fa-handshake"></i>
+              <small>Appointment</small>
+            </div>
+            <div class="icon-circle">
+              <small>Applicant Pool</small>
+              <i class="fa fa-users"></i>
+            </div>
+          </div>
+
+          <div class="icons d-flex align-items-center">
+            <div class="icon-circle">
+              <small>Phone Screening</small>
+              <i class="fa fa-mobile"></i>
+            </div>
+            <div class="icon-circle">
+              <small
+                >2<sup>nd</sup> Interview (Managerial/Critical Postion)</small
+              >
+              <i class="fa fa-user-group"></i>
+            </div>
+            <div class="icon-circle">
+              <small>Offer Discussion</small>
+              <i class="fa fa-hand-holding-heart"></i>
+            </div>
+          </div>
+          <div class="apply-now-section p-2">
+            <h4 class="primary-color text-uppercase">Apply Now</h4>
+            <h4>
+              <a
+                class="text-dark text-decoration-none"
+                href="careers.ideas.com.pk"
+                >careers.ideas.com.pk</a
+              >
+            </h4>
+          </div>
+          <div class="qr-section">
+            <h6 class="text-uppercase">connect us</h6>
+            <img
+              src="{{ asset('images/qr-img.png') }}"
+              alt="Qr Image"
+              widdh="70"
+              height="70"
+            />
+          </div>
+        </div>
+      </div>
+      <div
+        class="left-box text-center d-flex align-items-center justify-content-center"
+      >
+        <div class="content">
+          <div class="heading">
+            <h5 class="text-uppercase">Join</h5>
+          </div>
+          <div class="ideas-logo">
+            <img src="{{ asset('images/logo-ideas.png') }}" alt="ideas logo" />
+          </div>
+          <div class="heading">
+            <h5 class="text-uppercase">Achieve</h5>
+          </div>
+          <div class="highlighted-text primary-bg text-white">
+            <h6 class="text-uppercase">A great career in fashion retail</h6>
+          </div>
+          <div class="website">
+            <h4>
+              <a class="text-dark text-decoration-none" href="ideas.com.pk">
+                ideas.com.pk</a
+              >
+            </h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
